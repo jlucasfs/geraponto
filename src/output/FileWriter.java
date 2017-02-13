703f69a2e0a91a5fa23cec6a14b9aa02ac5fa4b8
thiago.lima <thiago.lima@ansnotehp022330.ans.gov.br>
2017-02-13 17:12:05 -0200

    Preenchimento de horas por data para cada colaborador no arquivo excel

--- src/output/FileWriter.java
+++ src/output/FileWriter.java
@@ -7,6 +7,8 @@ import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Locale;
@@ -18,13 +20,12 @@ import org.apache.poi.ss.usermodel.Cell;
 
 import model.Collaborator;
 
-
 /**
  * @author thiago.lima
  *
  */
 public class FileWriter {
-	
+
 	private static final Integer INITIAL_CELL = 0;
 	private static final Integer INITIAL_ROW = 0;
 	private static final String SHEET_TITLE = "Folha de Ponto ";
@@ -32,16 +33,16 @@ public class FileWriter {
 	private static final String COLUMN_NAME = "Nome:";
 	private static final String COLUMN_DATE = "Data";
 	private static final String COLUMN_HOUR = "Hora ";
-	private static final String DATE_FORMAT = "dd-MM-yy HHmm";
+	private static final String DATE_FORMAT = "dd/MM/yyyy";
+	private static final String HOUR_FORMAT = "HH:mm";
+	private static final String DATE_HOUR_FORMAT = "dd-MM-yy HHmm";
 	private static final String FILE_EXTENSION = ".xls";
-		
-	
+
 	private HSSFWorkbook workbook;
 	private Integer maxColumn;
 	private Integer columnNumber;
 	private Integer rowNumber;
-	
-	
+
 	public FileWriter() {
 		super();
 		this.workbook = new HSSFWorkbook();
@@ -49,9 +50,9 @@ public class FileWriter {
 		this.rowNumber = INITIAL_ROW;
 	}
 
-	
 	/**
 	 * Processa a lista de colaboradores e horários
+	 * 
 	 * @param collabList
 	 */
 	public void processFile(HashSet<Collaborator> collabList) {
@@ -60,57 +61,59 @@ public class FileWriter {
 			HSSFSheet sheet = getWorkbook().createSheet(collaborator.getPis().toString());
 			createSheet(sheet, collaborator);
 		}
-		
+
 		String filename = generateFileName();
 		try {
 			saveFile(filename);
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Cria uma nova pasta de planilha para cada {@link Collaborator}
+	 * 
 	 * @param sheet
 	 * @param collaborator
 	 */
 	public void createSheet(HSSFSheet sheet, Collaborator collaborator) {
-		
+
 		createHeader(sheet, collaborator);
 		createTimeSheet(sheet, collaborator);
-		
+
 	}
-	
+
 	/**
 	 * Cria o cabeçalho com informações pessoais do {@link Collaborator}
+	 * 
 	 * @param sheet
 	 * @param collaborator
 	 */
 	public void createHeader(HSSFSheet sheet, Collaborator collaborator) {
-		
-		HSSFRow row = sheet.createRow(INITIAL_ROW);		
+
+		HSSFRow row = sheet.createRow(INITIAL_ROW);
 		Cell cell = row.createCell(INITIAL_CELL);
 		cell.setCellValue(COLUMN_PIS);
 		cell = row.createCell(INITIAL_CELL + 1);
 		cell.setCellValue(collaborator.getPis());
-		
+
 		row = sheet.createRow(sheet.getLastRowNum() + 1);
 		cell = row.createCell(INITIAL_CELL);
 		cell.setCellValue(COLUMN_NAME);
 		cell = row.createCell(INITIAL_CELL + 1);
 		cell.setCellValue(collaborator.getName());
-		
+
 		// Insere uma linha vazia para separação
 		row = sheet.createRow(sheet.getLastRowNum() + 1);
-		
+
 	}
-	
+
 	/**
 	 * Cria uma linha para cada dia trabalhado, listando as marcações de ponto por hora
 	 * @param sheet
@@ -118,40 +121,82 @@ public class FileWriter {
 	 */
 	public void createTimeSheet(HSSFSheet sheet, Collaborator collaborator) {
 		setColumnNumber(INITIAL_CELL);
+		setMaxColumn(INITIAL_CELL);
+		Integer dateComparator = 1;
+		DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+		DateTimeFormatter hourFormatter = DateTimeFormatter.ofPattern(HOUR_FORMAT);
 		
-		HSSFRow row = sheet.createRow(sheet.getLastRowNum() + 1);
+		
+		// Armazena a linha do cabeçalho
+		setRowNumber(sheet.getLastRowNum());
+		
+		// Cabeçalho
+		HSSFRow row = sheet.createRow(sheet.getLastRowNum());
 		Cell cell = row.createCell(getColumnNumber());
 		cell.setCellValue(COLUMN_DATE);
 		cell = row.createCell(getColumnNumber() + 1);
 		cell.setCellValue(COLUMN_HOUR + "1");
 		cell = row.createCell(getColumnNumber() + 2);
 		cell.setCellValue(COLUMN_HOUR + "2");
+		cell = row.createCell(getColumnNumber() + 3);
+		cell.setCellValue(COLUMN_HOUR + "3");
+		cell = row.createCell(getColumnNumber() + 4);
+		cell.setCellValue(COLUMN_HOUR + "4");
+		setMaxColumn(getColumnNumber()  + 4);
 		
-		row = sheet.createRow(sheet.getLastRowNum() + 1);
-		cell = row.createCell(getColumnNumber());
-		cell.setCellValue("13/02/17");
-		cell = row.createCell(getColumnNumber() + 1);
-		cell.setCellValue("09:00");
-		cell = row.createCell(getColumnNumber() + 2);
-		cell.setCellValue("12:00");
-		
-//		HashSet<LocalDateTime> timetable = collaborator.getTimetable();
+		// Loop para cada entrada de hora de cada colaborador
+		for (LocalDateTime hora : collaborator.getTimetable()) {
+			
+			if (hora.getDayOfYear() != dateComparator) {
+				setColumnNumber(INITIAL_CELL);
+				
+				// Se a data mudou, cria uma nova linha
+				row = sheet.createRow(sheet.getLastRowNum() + 1);
+				// Preenche data
+				cell = row.createCell(getColumnNumber());
+				cell.setCellValue(hora.format(dateFormatter));
+				
+				// Preenche entrada de hora
+				cell = row.createCell(getColumnNumber() + 1);
+				cell.setCellValue(hora.format(hourFormatter));
+
+			} else {
+			
+				// Se a data não mudou, inclui hora na proxima coluna
+				cell = row.createCell(getColumnNumber() + 1);
+				cell.setCellValue(hora.format(hourFormatter));
+			}
+			
+			
+			// Inclui cabeçalho para marcações além do limite padrão (4)
+			if (getColumnNumber() > getMaxColumn()) {
+				row = sheet.getRow(getRowNumber());
+				cell = row.createCell(getMaxColumn() + 1);
+				cell.setCellValue(COLUMN_HOUR + (getMaxColumn() + 1));
+				
+				// Retorna para a ultima linha e celula para continuar preenchimento
+				setMaxColumn(getMaxColumn() + 1);
+				row = sheet.getRow(sheet.getLastRowNum());
+			}
+			
+			setColumnNumber(getColumnNumber() + 1);
+			dateComparator = hora.getDayOfYear();
+		}
 		
 	}
-	
+
 	public String generateFileName() {
-		SimpleDateFormat format = new SimpleDateFormat(DATE_FORMAT, Locale.getDefault());
+		SimpleDateFormat format = new SimpleDateFormat(DATE_HOUR_FORMAT, Locale.getDefault());
 		Date today = new Date();
 		String fileName = SHEET_TITLE + format.format(today) + FILE_EXTENSION;
-		
+
 		return fileName;
 	}
-	
+
 	public void saveFile(String fileName) throws IOException {
 		FileOutputStream out = new FileOutputStream(System.getProperty("user.home") + "\\Documents\\" + fileName);
 		getWorkbook().write(out);
 	}
-	
 
 	/**
 	 * @return the workbook
@@ -161,7 +206,8 @@ public class FileWriter {
 	}
 
 	/**
-	 * @param workbook the workbook to set
+	 * @param workbook
+	 *            the workbook to set
 	 */
 	public void setWorkbook(HSSFWorkbook workbook) {
 		this.workbook = workbook;
@@ -175,12 +221,12 @@ public class FileWriter {
 	}
 
 	/**
-	 * @param maxColumn the maxColumn to set
+	 * @param maxColumn
+	 *            the maxColumn to set
 	 */
 	private void setMaxColumn(Integer maxColumn) {
 		this.maxColumn = maxColumn;
 	}
-	
 
 	/**
 	 * @return the columnNumber
@@ -190,7 +236,8 @@ public class FileWriter {
 	}
 
 	/**
-	 * @param columnNumber the columnNumber to set
+	 * @param columnNumber
+	 *            the columnNumber to set
 	 */
 	public void setColumnNumber(Integer columnNumber) {
 		this.columnNumber = columnNumber;
@@ -204,7 +251,8 @@ public class FileWriter {
 	}
 
 	/**
-	 * @param rowNumber the rowNumber to set
+	 * @param rowNumber
+	 *            the rowNumber to set
 	 */
 	public void setRowNumber(Integer rowNumber) {
 		this.rowNumber = rowNumber;
