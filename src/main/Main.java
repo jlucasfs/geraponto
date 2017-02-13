48e98e6a355ff2d74542c1c1b35179fae78e4919
joao.sarmento <joao.sarmento@ansnotehp022342.ans.gov.br>
2017-02-13 15:56:34 -0200

    criacao tela de importacao arquivo

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,43 +1,165 @@
-/**
- * 
- */
 package main;
 
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.IOException;
 import java.util.HashSet;
+
+import javax.swing.AbstractAction;
+import javax.swing.Action;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+import javax.swing.border.EmptyBorder;
 
 import input.FileReader;
 import model.Collaborator;
 import output.FileWriter;
+import util.Constantes;
 
 /**
  * @author thiago.lima
  *
  */
-public class Main {
+public class Main extends JFrame {
 
 	static HashSet<Collaborator> collaborators;
 
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
+	public static void mainOld(String[] args) {
 
 		try {
 			collaborators = FileReader.fillTimeTable();
 			FileWriter fileWriter = new FileWriter();
 			fileWriter.processFile(collaborators);
-					
-			//imprimindo lista
-//			for (Collaborator c : collaborators) {
-//
-//				System.out.println(c.getName());
-//				c.getTimetable().forEach(tt -> System.out.println(tt.toString()));
-//			}
+
+			// imprimindo lista
+			// for (Collaborator c : collaborators) {
+			//
+			// System.out.println(c.getName());
+			// c.getTimetable().forEach(tt ->
+			// System.out.println(tt.toString()));
+			// }
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private JPanel contentPane;
+
+	private JDesktopPane desktopPane = new JDesktopPane();
+
+	// Menus
+	private JMenu mnCadastro = new JMenu("Cadastro");
+	private JMenuItem mntmColaborador = new JMenuItem("Colaborador");
+	private JMenuItem mntmImportacao = new JMenuItem("Importar marcacoes");
+
+	// Actions
+	private final Action itemColaboradorAction = new ItemColaboradorAction();
+	private final Action itemImportacaoAction = new ItemImportacaoAction();
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			// HibernateUtil.initialize();
+			UIManager.setLookAndFeel(Constantes.LOOK_AND_FEEL_NIMBUS);
+		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException
+				| UnsupportedLookAndFeelException e1) {
+			e1.printStackTrace();
+		}
+		EventQueue.invokeLater(new Runnable() {
+
+			public void run() {
+				try {
+					Main frame = new Main();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Main() {
+		addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowOpened(WindowEvent e) {
+				setExtendedState(MAXIMIZED_BOTH);
+			}
+		});
+		setTitle(Constantes.APP_TITLE);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 680, 526);
+
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+
+		menuBar.add(mnCadastro);
+
+		mntmColaborador.setAction(itemColaboradorAction);
+		mnCadastro.add(mntmColaborador);
+
+		mnCadastro.addSeparator();
+
+		mntmImportacao.setAction(itemImportacaoAction);
+		mnCadastro.add(mntmImportacao);
+
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+
+		contentPane.add(desktopPane, BorderLayout.CENTER);
+		desktopPane.setLayout(null);
+	}
+
+	private class ItemColaboradorAction extends AbstractAction {
+
+		private static final long serialVersionUID = 8889834435576927855L;
+
+		public ItemColaboradorAction() {
+			putValue(NAME, "Colaborador");
+			putValue(SHORT_DESCRIPTION, "Cadastro de Colaborador");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			ColaboradorView item = new ColaboradorView(desktopPane);
+			desktopPane.add(item);
+			item.setVisible(true);
+		}
+	}
+
+	private class ItemImportacaoAction extends AbstractAction {
+
+		private static final long serialVersionUID = -6721667389598666270L;
+
+		public ItemImportacaoAction() {
+			putValue(NAME, "Importar Marcações");
+			putValue(SHORT_DESCRIPTION, "Importar marcações de ponto");
+		}
+
+		public void actionPerformed(ActionEvent e) {
+			ImportacaoView item = new ImportacaoView(desktopPane);
+			desktopPane.add(item);
+			item.setVisible(true);
+		}
 	}
 
 }
